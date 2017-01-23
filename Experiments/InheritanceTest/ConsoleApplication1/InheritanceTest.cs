790c34027fd2861d4460b0ab01269dc1aae0d2b3
Sean Hinchee <seh@iastate.edu>
2017-01-23 09:49:21 -0600

    pushing c# test from 1/23's morning

---
+++ Experiments/InheritanceTest/ConsoleApplication1/InheritanceTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InheritanceTest
+{
+    class InheritanceTest
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test 1 2 3");
+            Console.ReadLine();
+
+            bool tf = true;
+            int num = 102;
+            string str = "a new string is returned if changed, strings are immutable";
+            char c = '$';
+            float f = 3.141596F; /* without the trailing F, the numerical portion is defined as a double */
+
+            Console.WriteLine("Insight: " + str);
+
+            Console.Write("Name?: ");
+            string name = Console.ReadLine();
+
+            Console.ReadLine();
+
+            int a, b;
+            Console.Write("Num 1: ");
+            a = int.Parse(Console.ReadLine());
+            Console.Write("Num 2: ");
+            b = int.Parse(Console.ReadLine());
+
+            Console.ReadLine();
+
+            /* if/else statements are generic and normal as well as switch/case statements (require breaks) */
+
+            ArrayList list = new ArrayList();
+            list.Add("Super");
+            list.Add("Grim");
+            list.Add("Dark");
+
+            foreach (string s in list)
+                Console.WriteLine("In list: " + s);
+
+            Console.ReadLine();
+        }
+    }
+}
