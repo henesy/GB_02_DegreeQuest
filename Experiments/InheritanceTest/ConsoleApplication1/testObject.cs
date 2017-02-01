351dd6a91823a8dfbc86a85b2318677285b7462b
dgxu <dgxu@iastate.edu>
2017-01-31 18:11:56 -0600

    git-lab2 commit

---
+++ Experiments/InheritanceTest/ConsoleApplication1/testObject.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class testObject
+    {
+        public testObject(int test, ref int ptr, string sd)
+        {
+
+        }
+         
+
+         
+
+    
+    }
+}
