b69bb6f8bc56a1e54d827f1a34546025be296b01
Zachary Boe <zachboe@iastate.edu>
2017-03-21 17:36:38 -0500

    added ScreenManager and GameScreen

---
+++ DegreeQuest/GameScreen.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics; 
+
+/*
+ * this is a interface that all screens in the game will use
+ */
+namespace DegreeQuest
+{
+    class GameScreen
+    {
+        protected ContentManager content;
+
+        public virtual void Initialize() { }
+        public virtual void LoadContent(ContentManager c)
+        {
+            content = new ContentManager(c.ServiceProvider, "Content");
+        }
+        public virtual void UnloadContent()
+        {
+            content.Unload();
+        }
+        public virtual void Draw(SpriteBatch sb) { }
+        public virtual void Update(GameTime gt) { }
+    }
+}
