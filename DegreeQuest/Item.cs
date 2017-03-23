85c9a32c37e1970b2103cb8c38cc4953037613c5
Zach Turley <zturley@iastate.edu>
2017-03-23 15:28:40 -0500

    Added NPC and Item spawning for server  on F4 and F3(defaults)

--- DegreeQuest/Item.cs
+++ DegreeQuest/Item.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/* below are added for MonoGame purposes */
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System.Runtime.Serialization;
 
 namespace DegreeQuest
 {
-    class Item
+
+    [Serializable()]
+    public class Item : Actor
     {
         public static String name_DFLT = "thing";
         public static int type_DFLT = 0;
@@ -18,7 +24,7 @@ namespace DegreeQuest
         public static int chem_DFLT = 0;
         public static int tech_DFLT = 0;
 
-        public string name;
+        public String name;
 
         public int type, value, effect, rarity;
         
@@ -28,6 +34,8 @@ namespace DegreeQuest
         //Current default constructor
         public Item()
         {
+            Texture = "DiamondSword";
+            name = name_DFLT;
             type = value = effect = rarity = 0;
             stats = new int[Stat.NUM];
             for(int i=0;i<stats.Length;i++)
@@ -35,5 +43,10 @@ namespace DegreeQuest
                 stats[i] = 0;
             }
         }
+
+        public override AType GetAType() { return AType.Item; }
+
+        public override Vector2 GetPos()
+        { return Position.toVector2(); }
     }
 }
